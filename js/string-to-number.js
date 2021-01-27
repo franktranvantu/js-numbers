0bf86b728ac05740474008692c3956835531d5e4
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-27 10:41:11 +0700

    Update

--- js/string-to-number.js
+++ js/string-to-number.js
@@ -1,6 +1,13 @@
 const HTMLBadges = prompt('How many HTML badges do you have?');
 const CSSBadges = prompt('How many CSS badges do you have?');
 
-const totalBadges = HTMLBadges + CSSBadges;
+// parseInt(), parseFloat()
+const totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
 console.log(totalBadges);
-console.log(typeof HTMLBadges, typeof CSSBadges);
+
+// +
+const totalBadges = +HTMLBadges + +CSSBadges;
+console.log(totalBadges);
+
+const pi = '3.14';
+console.log(+pi === 3.14)
