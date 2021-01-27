852dad1dc5db653f51b575fbe50f59e339acf776
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-27 10:19:02 +0700

    Update

---
+++ js/math.js
@@ -0,0 +1,11 @@
+const secsPerMin = 60;
+const minsPerHour = 60;
+const hoursPerDay = 24;
+const daysPerWeek = 7;
+const weeksPerYear = 52;
+
+const secsPerDay = secsPerMin * minsPerHour * hoursPerDay;
+console.log(`There are ${secsPerDay} seconds in a day.`)
+
+const yearsAlive;
+console.log(`I've been alive for more than [x] seconds!`);
